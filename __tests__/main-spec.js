30a2cd6b0f48b3d5bb97a1d219645d4c62358c33
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-20 17:36:19 +0800

    task diagrams

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -3,3 +3,13 @@ const add = require('../main');
 it ('should add two numbers', () => {
     expect(add(2, 3)).toBe(5);
 });
+
+it ('should return multiply tabale', () => {
+//given
+const firstParameter = 5;
+const secondParameter = 3;
+//when
+const result = checkLegalInput(firstParameter,secondParameter);
+//then
+    expect(result).toBe(null);
+}); 
