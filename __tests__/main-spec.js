a0ae83857957e6e367a20884875bd4722062429d
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-21 19:36:41 +0800

    test1

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -22,3 +22,24 @@ it('should generate multiply array', () => {
     expect(result[1][0]).toBe("2*3=6");
     expect(result[1][1]).toBe("3*3=9");
 }); 
+
+it('should return string', () => {
+    //given
+    let array = [["2*2=4"],
+    ["2*3=6","3*3=9"]
+    ];
+    //when
+    const result = printMultiplyTable(array);
+    //then
+    expect(result).toBe("2*2=4 \n2*3=6 3*3=9 \n");
+}); 
+
+it('should print', () => {
+    //given
+    let firstNum = 2;
+    let secondNum = 3;
+    //when
+    const result = multiplyTable(firstNum, secondNum);
+    //then
+    expect(result).toBe("2*2=4 \n2*3=6 3*3=9 \n");
+});
--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 
 function multiplyTable(firstNum, secondNum) {
-
-
+    let result;
+    let generateArray
+    if (isValid(firstNum, secondNum)) {
+        generateArray = generateMultiplyArray(firstNum, secondNum);
+        result = printMultiplyTable(generateArray);
+        return result;
+    } else {
+        return null;
+    }
 }
 
 function isValid(firstNum, secondNum) {
@@ -10,12 +17,11 @@ function isValid(firstNum, secondNum) {
     return result
 }
 
-
 function generateMultiplyArray(firstNum, secondNum) {
     let generateArray = [];
     for (let i = 0; i < (secondNum - firstNum + 1); i++) {
         let row = [];
-        for (let j = 0; j < (i+1); j++) {
+        for (let j = 0; j < (i + 1); j++) {
             let result = (firstNum + j) * (firstNum + i);
             let str = (firstNum + j) + "*" + (firstNum + i) + "=" + result;
             row.push(str);
@@ -25,10 +31,15 @@ function generateMultiplyArray(firstNum, secondNum) {
     return generateArray;
 }
 
-
 function printMultiplyTable(generateArray) {
-
+    let result = "";
+    for (let i = 0; i < generateArray.length; i++) {
+        for (let j = 0; j < generateArray[i].length; j++) {
+            result = result + generateArray[i][j] + " ";
+        }
+        result = result + "\n";
+    }
+    return result;
 }
-
 
 module.exports = { isValid, generateMultiplyArray, printMultiplyTable, multiplyTable };
