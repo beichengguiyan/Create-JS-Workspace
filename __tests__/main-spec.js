63b4ffff13f753065a15e5cea252fecd8c622724
beichengguiyan <chihaotian@zybank.com.cn>
2019-08-21 18:37:03 +0800

    1

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,15 +1,24 @@
-const add = require('../main');
+const { isValid, generateMultiplyArray, printMultiplyTable, multiplyTable } = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+
+it('should number be valid', () => {
+    //given
+    let firstNum = 2;
+    let secondNum = 3;
+    //when
+    const result = isValid(firstNum, secondNum);
+    //then
+    expect(result).toBe(true);
 });
 
-it ('should return multiply tabale', () => {
-//given
-const firstParameter = 5;
-const secondParameter = 3;
-//when
-const result = checkLegalInput(firstParameter,secondParameter);
-//then
-    expect(result).toBe(null);
+it('should generate multiply array', () => {
+    //given
+    let firstNum = 2;
+    let secondNum = 3;
+    //when
+    const result = generateMultiplyArray(firstNum, secondNum);
+    //then
+    expect(result[0][0]).toBe("2*2=4");
+    expect(result[1][0]).toBe("2*3=6");
+    expect(result[1][1]).toBe("3*3=9");
 }); 
--- main.js
+++ main.js
@@ -1,5 +1,34 @@
-function add (left, right) {
-    return left + right;
+
+function multiplyTable(firstNum, secondNum) {
+
+
 }
 
-module.exports = add;
+function isValid(firstNum, secondNum) {
+    let result;
+    result = firstNum <= secondNum;
+    return result
+}
+
+
+function generateMultiplyArray(firstNum, secondNum) {
+    let generateArray = [];
+    for (let i = 0; i < (secondNum - firstNum + 1); i++) {
+        let row = [];
+        for (let j = 0; j < (i+1); j++) {
+            let result = (firstNum + j) * (firstNum + i);
+            let str = (firstNum + j) + "*" + (firstNum + i) + "=" + result;
+            row.push(str);
+        }
+        generateArray.push(row);
+    }
+    return generateArray;
+}
+
+
+function printMultiplyTable(generateArray) {
+
+}
+
+
+module.exports = { isValid, generateMultiplyArray, printMultiplyTable, multiplyTable };
